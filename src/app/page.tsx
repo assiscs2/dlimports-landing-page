c68f86c5fd77caeac2667888fea6e540fe074eea
Gustavo Assis <assiscs2@gmail.com>
2023-08-04 17:16:09 -0300

    first commit for vercel deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,110 @@
-import Image from 'next/image'
+import Image from "next/image";
+import logoDLimports from "../assets/logo-dlimports.webp"
+import logoInstagram from '../assets/instagram-logo.svg'
+import logoFacebook from '../assets/facebook-logo.svg'
+import { ContactButton } from "@/components/ContactButton";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    <main className="flex items-center text-gray-950 flex-col justify-center text-sm min-w-full lg:text-base overflow-hidden antialiased">
+      {/* <head>
+      <meta name="facebook-domain-verification" content="1mg6s10ngu5mmjbhrnuvq9j3u4fijh" />
+    </head> */}
+      <div className="w-full">
+
+        <header className="bg-[#7f11BB] h-[4rem] flex items-center justify-center text-yellow-200 lg:h-24">
+          <a href="https://www.dlimport.com.br/"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            <Image src={logoDLimports} height={120} width={240} alt="logo Maloni Gold" className="h-12 w-auto" />
           </a>
+        </header>
+        <div
+          className="bg-gray-300"
+        >
+
+          <div className="flex flex-col items-center justify-center w-full mx-auto max-w-[820px]">
+            <div className="text-center pt-4 px-4 w-full">
+              <h1 className="text-[#7f11BB] font-extrabold text-xl underline underline-offset-2 block">Aproveitem!!</h1>
+              <p className="text-[#7f11BB] font-extrabold text-base underline-offset-2 inline">As melhores marcas, com os menores preços!</p>
+              <p className="mt-2">Na compra de um perfume Ferrari Black ou Fantasy Britney Spears, receba em sua casa com <p className="text-[#7f11BB] font-extrabold underline underline-offset-2 inline"> frete grátis</p> para São Paulo!</p>
+              <p>Mas não deixe para depois!! A promoção válida apenas para às 100 primeiras unidades!</p>
+            </div>
+            <div className="flex flex-col w-full h-auto justify-center items-center">
+              <div className="flex gap-2">
+                <div className=" w-full h-auto flex flex-col px-4 py-2 gap-4 ">
+                  <div className="w-auto">
+                    <div className="bg-gray-600 h-44 w-auto flex items-center justify-center mt-2">
+                      IMAGEM
+                    </div>
+                    <div className="text-center flex flex-col gap-1 mt-3">
+                      <p>
+                        Ferrari Black Eau de Toilette - Perfume Masculino 125ml
+                      </p>
+                      <p>Perfume masculino amadeirado e cítrico. Da linha Scuderia Ferrari, sua fragrância é esportiva e sofisticada.</p>
+                      <p className="text-[#7f11BB] font-extrabold text-base underline underline-offset-2">
+                        Por apenas R$159,99!!
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-auto">
+                    <div className="bg-gray-600 h-44 w-auto flex items-center justify-center mt-2">
+                      IMAGEM
+                    </div>
+                    <div className="text-center flex flex-col gap-1 mt-3">
+                      <p>
+                        Fantasy Britney Spears Eau de Parfum - Perfume Feminino 100ml
+                      </p>
+                      <p>Perfume feminino sexy e descolado. Com frutas, perfeito para a mulher romântica, mas que adora provocar.</p>
+                      <p className="text-[#7f11BB] font-extrabold text-base underline underline-offset-2">
+                        Por apenas R$189,99!!
+                      </p>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+              <div>
+                <div className="px-4 flex items-center justify-center">
+                  < ContactButton />
+                </div>
+                <div className="text-center pt-4 px-4 w-full mx-auto max-w-[820px] p">
+                  <div className="bg-gray-600 h-44 w-auto flex items-center justify-center">
+                    IMAGEM
+                  </div>
+                  <div>
+                    <p className="mt-4">
+                      A <a href="https://www.dlimport.com.br/"><p className="text-[#7f11BB] font-extrabold text-base underline underline-offset-2 inline">DL IMPORTS</p></a> foi fundada em 2015, buscando trazer o melhor custo e benefício, com preços justos em produtos das melhores e mais queridas marcas do mercado!
+                      <p>Oferecendo ampla variedade, qualidade e excelência, confira nossas opções e com os melhores preços!!</p>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p className="text-center text-gray-700 translate-y-2">Redes sociais:</p>
+          <div className="flex gap-2 justify-center items-center p-2">
+
+            <a href="" target="_blank">
+              <Image src={logoInstagram} height={48} width={48} alt="Logo do instagram" className="max-h-[3rem] max-w-[3rem]" />
+            </a>
+            <a href="" target="_blank">
+              <Image src={logoFacebook} height={48} width={48} alt="Logo do facebook" className="max-h-[2.8rem] max-w-[2.8rem]" />
+            </a>
+          </div>
+
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      <footer className="bg-[#952cce] p-4 flex items-center justify-center w-full">
+        <a href="https://www.dlimport.com.br/"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+          <p className="font-bold text-gray-100">
+            DLImports
           </p>
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </footer>
     </main>
   )
 }
