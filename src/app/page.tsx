cdb8de0a71fd9fdd6cb4e629aac53138cfa51e03
Gustavo Assis <assiscs2@gmail.com>
2023-08-08 16:05:00 -0300

    initial product images layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import logoDLimports from "../assets/logo-dlimports.webp"
 import logoInstagram from '../assets/instagram-logo.svg'
-import logoFacebook from '../assets/facebook-logo.svg'
+// import logoFacebook from '../assets/facebook-logo.svg'
 import { ContactButton } from "@/components/ContactButton";
+import imageFerrariBlack from "../assets/FerrariBlack.webp"
+import imageBritneySpears from "../assets/BritneySpears.jpg"
 
 
 export default function Home() {
@@ -17,11 +19,11 @@ export default function Home() {
           <a href="https://www.dlimport.com.br/"
             target="_blank"
           >
-            <Image src={logoDLimports} height={120} width={240} alt="logo Maloni Gold" className="h-12 w-auto" />
+            <Image src={logoDLimports} height={120} width={240} alt="logo DLimports" className="h-12 w-auto" />
           </a>
         </header>
         <div
-          className="bg-gray-300"
+          className="bg-gray-200"
         >
 
           <div className="flex flex-col items-center justify-center w-full mx-auto max-w-[820px]">
@@ -31,14 +33,14 @@ export default function Home() {
               <p className="mt-2">Na compra de um perfume Ferrari Black ou Fantasy Britney Spears, receba em sua casa com <p className="text-[#7f11BB] font-extrabold underline underline-offset-2 inline"> frete grátis</p> para São Paulo!</p>
               <p>Mas não deixe para depois!! A promoção válida apenas para às 100 primeiras unidades!</p>
             </div>
-            <div className="flex flex-col w-full h-auto justify-center items-center">
+            <div className="flex flex-col w-full h-auto justify-center items-center ">
               <div className="flex gap-2">
                 <div className=" w-full h-auto flex flex-col px-4 py-2 gap-4 ">
                   <div className="w-auto">
-                    <div className="bg-gray-600 h-44 w-auto flex items-center justify-center mt-2">
-                      IMAGEM
+                    <div className="bg-white w-[100vw] h-[340px] md:h-[488px] lg:h-[500px] flex items-center justify-center mt-2 p-1 overflow-hidden">
+                      <Image src={imageFerrariBlack} height={400} width={400} alt="Imagem de um Perfume Ferrari Black Eau de Toilette" className="h-80 w-80 md:h-fit md:w-fit lg:h-fit lg:w-fit max-h-[500px]"/>
                     </div>
-                    <div className="text-center flex flex-col gap-1 mt-3">
+                    <div className="text-center flex flex-col gap-1 mt-3 px-4">
                       <p>
                         Ferrari Black Eau de Toilette - Perfume Masculino 125ml
                       </p>
@@ -49,10 +51,10 @@ export default function Home() {
                     </div>
                   </div>
                   <div className="w-auto">
-                    <div className="bg-gray-600 h-44 w-auto flex items-center justify-center mt-2">
-                      IMAGEM
+                  <div className="bg-white w-[100vw] h-[340px] md:h-[488px] lg:h-[500px] flex items-center justify-center mt-2 p-1 overflow-hidden">
+                      <Image src={imageBritneySpears} height={400} width={400} alt="Imagem de um Perfume Spears Eau de Parfum" className="h-[400px] h-[400px] overflow-hidden md:h-fit md:w-fit lg:h-fit lg:w-fit max-h-[640px]"/>
                     </div>
-                    <div className="text-center flex flex-col gap-1 mt-3">
+                    <div className="text-center flex flex-col gap-1 mt-3 px-4">
                       <p>
                         Fantasy Britney Spears Eau de Parfum - Perfume Feminino 100ml
                       </p>
@@ -70,9 +72,9 @@ export default function Home() {
                   < ContactButton />
                 </div>
                 <div className="text-center pt-4 px-4 w-full mx-auto max-w-[820px] p">
-                  <div className="bg-gray-600 h-44 w-auto flex items-center justify-center">
-                    IMAGEM
-                  </div>
+                <div className="bg-gray-100 w-auto flex items-center justify-center mt-2 p-1">
+                        Imagem
+                    </div>
                   <div>
                     <p className="mt-4">
                       A <a href="https://www.dlimport.com.br/"><p className="text-[#7f11BB] font-extrabold text-base underline underline-offset-2 inline">DL IMPORTS</p></a> foi fundada em 2015, buscando trazer o melhor custo e benefício, com preços justos em produtos das melhores e mais queridas marcas do mercado!
@@ -86,12 +88,12 @@ export default function Home() {
           <p className="text-center text-gray-700 translate-y-2">Redes sociais:</p>
           <div className="flex gap-2 justify-center items-center p-2">
 
-            <a href="" target="_blank">
+            <a href="https://www.instagram.com/dl.imports.oficial/" target="_blank">
               <Image src={logoInstagram} height={48} width={48} alt="Logo do instagram" className="max-h-[3rem] max-w-[3rem]" />
             </a>
-            <a href="" target="_blank">
+            {/* <a href="" target="_blank">
               <Image src={logoFacebook} height={48} width={48} alt="Logo do facebook" className="max-h-[2.8rem] max-w-[2.8rem]" />
-            </a>
+            </a> */}
           </div>
 
         </div>
